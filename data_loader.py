3e19945ebc4b08723a50bdf3e686c1964ad5f3e1
Mikk <kruusalu.mikk@gmail.com>
2023-11-07 09:02:45 +0100

    Added generate_neighbours.py

--- data_loader.py
+++
@@ -1,8 +0,0 @@
-# LOAD data here 
-def load_data():
-
-# Cutoff distance calucation
-def measure_cutoff():
-
-# save preprocessed dataset 
-def save_dataset():
---
+++ generate_neighbours.py
@@ -0,0 +1,34 @@
+import torch
+from torch_geometric.datasets import QM9
+from torch_geometric.data import Data
+
+k = 0
+def add_neighbours(data: Data, cutoff=5.0, padto=30) -> Data:
+    global k
+    print(f"Molecule {k}")
+    k += 1
+
+    natoms = data.pos.shape[0]
+
+    # i-th row is for i-th atom in data.z, ij element is distance between i-th and j-th atom
+    distances = torch.cdist(data.pos, data.pos, p=2)
+
+    # select atoms
+    neighbours = torch.where(distances <= cutoff, 1, 0)
+
+    # exclude itself
+    neighbours = neighbours - torch.eye(natoms)
+
+    # pad with zeros to have matching dimensions, otherwise pyg complains and won't save the data
+    neighbours = torch.cat((neighbours, torch.zeros((natoms, padto-natoms))), dim=1)
+
+    data.neighbours = neighbours
+    return data
+
+data = QM9(root="./data/5A", pre_transform=add_neighbours)
+
+print(data[2].neighbours)
+
+# max number of atoms -- 29
+# y = [data[i].z.shape[0] for i in range((130830))]
+# print(max(y))
