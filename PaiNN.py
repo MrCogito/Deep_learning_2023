228a15106398307e31a2427580204fbc7eb432b9
Mikk <kruusalu.mikk@gmail.com>
2023-11-18 16:46:29 +0100

    Update the model to use MessagePassing, message block working

--- PaiNN.py
+++ PaiNN.py
@@ -4,6 +4,82 @@ from torch.functional import F
 from torch_geometric.datasets import QM9
 from torch_geometric.loader import DataLoader
 from torch_geometric.utils import scatter
+from torch_geometric.nn import MessagePassing
+
+
+class Message(MessagePassing):
+    def __init__(self, num_embeddings, cutoff_dist):
+        super().__init__(flow="source_to_target")
+
+        self.num_embeddings = num_embeddings
+        self.cutoff_dist = cutoff_dist
+
+        self.linear_phi1 = nn.Linear(self.num_embeddings, self.num_embeddings)
+        self.linear_phi2 = nn.Linear(self.num_embeddings, 3*self.num_embeddings)
+        self.linear_W = nn.Linear(20, 3*self.num_embeddings)
+
+
+    # embeddings :      [natoms, num_embeddings]
+    # equivar_repr :    [3, natoms, num_embeddings]
+    # pos :             [natoms, 3]
+    # batch :           [natoms]
+    def forward(self, embeddings, equivar_repr, pos, batch):
+
+        # count atoms in each molecule
+        unique = torch.unique(batch, return_counts=True)
+        # i-th row is for i-th atom in data.z, ij element is distance between i-th and j-th atom
+        distances = torch.cdist(pos, pos, p=2)
+        # select atoms
+        neighbours = torch.where(distances <= self.cutoff_dist, 1, 0)
+        # mask other atoms
+        neighbours = neighbours * torch.block_diag(*[torch.ones((u,u)) for u in unique[1]])
+        # exclude itself
+        neighbours = neighbours - torch.eye(neighbours.shape[0])
+        # get neighbours in the form of [2, num_edges]
+        neighbours = neighbours.nonzero(as_tuple=False).t()
+        neighbours = torch.index_select(neighbours, 0, torch.LongTensor([1,0])).type(torch.LongTensor)
+
+        return self.propagate(neighbours, s=embeddings, v=equivar_repr, r=pos, neighbours=neighbours)
+
+    # _j is a neighbour, _i is the atom
+    # s_j : [num_edges, num_embeddings]
+    # v_j : [3, num_edges, num_embeddings]
+    # r_i : [num_edges, 3]
+    # r_j : [num_edges, 3]
+    def message(self, s_j, v_j, r_i, r_j, neighbours):
+        # 1. calculate phi
+        phi = self.linear_phi1(s_j) # [num_edges, num_embeddings]
+        phi = F.silu(phi)
+        phi = self.linear_phi2(phi) # [num_edges, 3*num_embeddings]
+
+        rel_pos = r_i - r_j         # [num_edges, 3]
+        distance = torch.norm(rel_pos, dim=1) # [num_edges]
+        RBF = torch.sin(torch.arange(1, 21)[None, :] * torch.pi * distance[:, None] / self.cutoff_dist) / distance[:, None] # [num_edges, 20]
+        W = self.linear_W(RBF)      # [num_edges, 3*num_embeddings]
+        # Add cosine cutoff         # [num_edges, 3*num_embeddings]
+
+        split = torch.mul(phi, W)   # [num_edges, 3*num_embeddings]
+
+        delta_s_ij = split[:, :self.num_embeddings] # [num_edges, num_embeddings]
+
+        v_j = v_j.permute([1,2,0]) # [num_edges, num_embeddings, 3]
+        delta_v_ij = torch.mul(v_j, split[:, self.num_embeddings:2*self.num_embeddings, None]) \
+                            + torch.mul(torch.mul(split[:, 2*self.num_embeddings:, None], rel_pos[:, None, :]), distance[:, None, None]) # [num_edges, num_embeddings, 3]
+
+        return delta_s_ij, delta_v_ij
+
+    def aggregate(self, message, s_j, v_j, r_i, r_j, neighbours):
+        delta_s_ij, delta_v_ij = message
+
+        delta_s_ij = scatter(delta_s_ij, neighbours[1], dim=0, reduce="sum") # [natoms, num_embeddings]
+        delta_v_ij = scatter(delta_v_ij, neighbours[1], dim=0, reduce="sum") # [natoms, num_embeddings, 3]
+
+        delta_v_ij = delta_v_ij.permute([2, 0, 1]) # [3, natoms, num_embeddings]
+
+        return delta_s_ij, delta_v_ij
+
+    def update(self, agg_message, s_j, v_j, r_i, r_j, neighbours):
+        return agg_message
 
 class PaiNN(nn.Module):
     def __init__(self, num_atoms, num_embeddings, cutoff_dist):
@@ -14,117 +90,61 @@ class PaiNN(nn.Module):
         self.embeddings = nn.Embedding(num_atoms, num_embeddings)
 
         # message block
-        self.linear_phi1 = nn.Linear(self.num_embeddings, self.num_embeddings)
-        self.linear_phi2 = nn.Linear(self.num_embeddings, 3*self.num_embeddings)
-        self.linear_W = nn.Linear(20, 3*self.num_embeddings)
+        self.message = Message(self.num_embeddings, cutoff_dist)
 
         # update block
-        self.linear_U = nn.Linear(self.num_embeddings, self.num_embeddings)
-        self.linear_V = nn.Linear(self.num_embeddings, self.num_embeddings)
-        self.linear_update1 = nn.Linear(2*self.num_embeddings, self.num_embeddings)
-        self.linear_update2 = nn.Linear(self.num_embeddings, 3*self.num_embeddings)
+        # self.linear_U = nn.Linear(self.num_embeddings, self.num_embeddings)
+        # self.linear_V = nn.Linear(self.num_embeddings, self.num_embeddings)
+        # self.linear_update1 = nn.Linear(2*self.num_embeddings, self.num_embeddings)
+        # self.linear_update2 = nn.Linear(self.num_embeddings, 3*self.num_embeddings)
 
-    # equivariant_repr: [n_neighbours, num_embeddings, 3]
-    # embeddings: [n_neighbours, num_embeddings]
-    # rel_pos: [n_neighbours, 3]
-    def message(self, equivariant_repr, embeddings, rel_pos):
-        phi = self.linear_phi1(embeddings) # [n_neighbours, num_embeddings]
-        phi = F.silu(phi)
-        phi = self.linear_phi2(phi)       # [n_neighbours, 3*num_embeddings]
+    # # equivariant_repr: [n_neighbours, num_embeddings, 3]
+    # # embeddings: [n_neighbours, num_embeddings]
+    # def update(self, equivariant_repr, embedding):
+    #     U = self.linear_U(equivariant_repr.permute(0,2,1))
+    #     U = U.permute(0,2,1)
+    #     print(f"U shape: ", U.shape)
+    #     V = self.linear_V(equivariant_repr.permute(0,2,1)) # [self.num_embeddings, 3]
+    #     V = V.permute(0,2,1)
+    #     print(V.shape)
+    #     print((torch.norm(V, dim=2)).shape)
 
-        distance = torch.norm(rel_pos, dim=1)   # [n_neighbours]
-        RBF = torch.stack([torch.sin(torch.arange(1, 21) * torch.pi * dist / self.cutoff_dist) / dist for dist in distance]) # [n_neighbours, 20]
-        W = self.linear_W(RBF) # [n_neighbours, 3*num_embeddings]
-        # Add cosine cutoff # [n_neighbours, 3*num_embeddings]
+    #     stack = torch.cat([embedding, torch.norm(V, dim=2)], dim=1)
+    #     print(f"stack.shape", stack.shape)
+    #     stack = self.linear_update1(stack)
+    #     stack = F.silu(stack)
+    #     split = self.linear_update2(stack)
+    #     split1 = split[:, :self.num_embeddings]  # First part, contains the first 128 elements in the second dimension
+    #     split2 = split[:, self.num_embeddings: 2*self.num_embeddings]  # Second part, contains the next 128 elements
+    #     split3 = split[:, 2*self.num_embeddings:]  # Third part, contains the last 128 elements
+    #     print(f"split1 size: ", split1.shape)
+    #     print(f"split2 size: ", split2.shape)
+    #     print(f"split3 size: ", split3.shape)
+    #     split1_expanded = split1.unsqueeze(-1)
+    #     equivariant_repr = torch.mul(U, split1_expanded)
+    #     print(torch.sum(U * V, dim=2).shape)
+    #     embedding = split2 + torch.sum(U * V, dim=2) \
+    #                  * split3
 
-        split = torch.mul(phi, W) # [n_neighbours, 3*num_embeddings]
-
-        embeddings = split[:, :self.num_embeddings] # [n_neighbours, num_embeddings]
-        embeddings = torch.sum(embeddings, dim=0)   # [num_embeddings]
-
-        equivariant_repr = torch.mul(equivariant_repr, split[:, self.num_embeddings:2*self.num_embeddings, None]) \
-                            + torch.mul(torch.mul(split[:, 2*self.num_embeddings:, None], rel_pos[:, None, :]), distance[:, None, None]) # [n_neighbours, num_embeddings, 3]
-        equivariant_repr = torch.sum(equivariant_repr, dim=0) # [num_embeddings, 3]
-
-        return equivariant_repr, embeddings
-
-    # equivariant_repr: [n_neighbours, num_embeddings, 3]
-    # embeddings: [n_neighbours, num_embeddings]
-    def update(self, equivariant_repr, embedding):
-        U = self.linear_U(equivariant_repr.permute(0,2,1))
-        U = U.permute(0,2,1)
-        print(f"U shape: ", U.shape)
-        V = self.linear_V(equivariant_repr.permute(0,2,1)) # [self.num_embeddings, 3]
-        V = V.permute(0,2,1)
-        print(V.shape)
-        print((torch.norm(V, dim=2)).shape)
-        
-        stack = torch.cat([embedding, torch.norm(V, dim=2)], dim=1)
-        print(f"stack.shape", stack.shape)
-        stack = self.linear_update1(stack)
-        stack = F.silu(stack)
-        split = self.linear_update2(stack)
-        split1 = split[:, :self.num_embeddings]  # First part, contains the first 128 elements in the second dimension
-        split2 = split[:, self.num_embeddings: 2*self.num_embeddings]  # Second part, contains the next 128 elements
-        split3 = split[:, 2*self.num_embeddings:]  # Third part, contains the last 128 elements
-        print(f"split1 size: ", split1.shape)
-        print(f"split2 size: ", split2.shape)
-        print(f"split3 size: ", split3.shape)
-        split1_expanded = split1.unsqueeze(-1)
-        equivariant_repr = torch.mul(U, split1_expanded)
-        print(torch.sum(U * V, dim=2).shape)
-        embedding = split2 + torch.sum(U * V, dim=2) \
-                     * split3
-
-        return equivariant_repr, embedding
+    #     return equivariant_repr, embedding
 
     def forward(self, data):
-        pos, z, neighbours = data.pos, data.z, data.neighbours
-
         # 1. Initialize inputs (s and v)
-        embeddings = self.embeddings(z) # [batch_size, num_embeddings]
-        equivariant_repr = torch.zeros((len(z), self.num_embeddings, 3))
-        print(embeddings.shape)
-        print('batch:', data.batch)
+        embeddings = self.embeddings(data.z) # [batch_size, num_embeddings]
+        equivariant_repr = torch.zeros((3, len(data.z), self.num_embeddings))
 
         # 2. Send messages and make updates
-        for i in torch.arange(len(z)): # for every atom
-            # get all atoms in the same molecule
-            atoms_ids = torch.where(torch.tensor(data.batch) == data.batch[i])[0]
-            atoms = data[atoms_ids]
-            #print(atoms)
-            # get neighbours of atom (ids) and their relative position
-            neighbours_idx = neighbours[i].nonzero(as_tuple=True)[0] # [num_neighbours]
-            rel_pos = pos[i] - pos
 
-            print(f'embeddings[{i}]:', embeddings[i].shape)
+        output = self.message(embeddings, equivariant_repr, data.pos, data.batch)
 
-            # send message (only to neighbours)
-            delta_v, delta_s = self.message(equivariant_repr[neighbours_idx], embeddings[neighbours_idx], rel_pos[neighbours_idx])
-            equivariant_repr[i] += delta_v
-            embeddings[i] += delta_s
-
-            #break
-
-            # update
-            # equivariant_repr[i], embeddings[i] += self.update(equivariant_repr[neighbours_idx], embeddings[neighbours_idx])
 
         # 3. Final linear layer
-        output = 0
+
         return output
 
-def create_dummy_data(num_neighbours, num_embeddings):
-    # Create a dummy equivariant representation tensor
-    equivariant_repr = torch.randn(num_neighbours, num_embeddings, 3)
-
-    # Create a dummy embedding tensor
-    embedding = torch.randn(num_neighbours, num_embeddings)
-
-    return equivariant_repr, embedding
 
 if __name__ == "__main__":
-    # cutoff_dist = 5
-
+    ### load data
     dataset = QM9(root=f"./data/{5}A")
 
     # Calculate split lengths
@@ -141,28 +161,15 @@ if __name__ == "__main__":
     val_loader = DataLoader(val_set, batch_size=32)
     test_loader = DataLoader(test_set, batch_size=32)
 
-    # net = PaiNN(9, 128, cutoff_dist)
-    # net(next(iter(train_loader))
-    num_neighbours = 5 # You can change this number to simulate different numbers of neighbours
-    num_embeddings = 128
 
-    #Create dummy data for dim testing
-    #equivariant_repr, embedding = create_dummy_data(num_neighbours, num_embeddings)
-
+    ### Testing
     # Instantiate the PaiNN model
-    model = PaiNN(num_atoms=9, num_embeddings=num_embeddings, cutoff_dist=5) # Adjust the parameters as needed
-
-    # Test dims in update block
-    # print(f"Equivariant size", equivariant_repr.shape)
-    # print(f"Embeddig size", embedding.shape)
-    # updated_equivariant_repr, updated_embedding = model.update(equivariant_repr, embedding)
-
-    # Testing
+    model = PaiNN(num_atoms=9, num_embeddings=128, cutoff_dist=5) # Adjust the parameters as needed
     batch = next(iter(train_loader))
     print(batch)
 
-    molecule = dataset[2]
-    model(batch)
+    out = model(batch)
+    print(out)
 
     # # Now you can check the dimensions and values of updated_equivariant_repr and updated_embedding
     # print("Updated Equivariant Representation Shape:", updated_equivariant_repr.shape)
