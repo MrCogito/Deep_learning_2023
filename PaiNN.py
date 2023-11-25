fa7ef0543984b5510c613e8817fdcf43c940ce1e
Mikk <kruusalu.mikk@gmail.com>
2023-11-25 19:38:17 +0200

    Different approach to messagepassing

--- PaiNN.py
+++ PaiNN.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.optim as optim
 from torch.functional import F
 from torch_geometric.datasets import QM9
 from torch_geometric.loader import DataLoader
@@ -132,6 +133,117 @@ class Update(GraphLayer):
 
         return delta_s_ij, delta_v_ij
 
+class MessageLayer(MessagePassing):
+    def __init__(self, num_embeddings, cutoff_dist, device):
+        super().__init__(flow="source_to_target")
+
+        self.device = device
+
+        self.num_embeddings = num_embeddings
+        self.cutoff_dist = cutoff_dist
+
+        self.linear_phi1 = nn.Linear(self.num_embeddings, self.num_embeddings)
+        self.linear_phi2 = nn.Linear(self.num_embeddings, 3*self.num_embeddings)
+        self.linear_W = nn.Linear(20, 3*self.num_embeddings)
+
+        self.linear_U = nn.Linear(self.num_embeddings, self.num_embeddings, bias=False)
+        self.linear_V = nn.Linear(self.num_embeddings, self.num_embeddings, bias=False)
+        self.linear_update1 = nn.Linear(2*self.num_embeddings, self.num_embeddings)
+        self.linear_update2 = nn.Linear(self.num_embeddings, 3*self.num_embeddings)
+
+    # embeddings :      [natoms, num_embeddings]
+    # equivar_repr :    [3, natoms, num_embeddings]
+    # pos :             [natoms, 3]
+    # batch :           [natoms]
+    def forward(self, embeddings, equivar_repr, pos, batch):
+        neighbours = self.get_neighbours_as_edge_index(pos, batch, self.cutoff_dist)
+
+        return self.propagate(neighbours, s=embeddings, v=equivar_repr, r=pos, neighbours=neighbours)
+
+    # _j is a neighbour, _i is the atom
+    # s_j : [num_edges, num_embeddings]
+    # v_j : [3, num_edges, num_embeddings]
+    # r_i : [num_edges, 3]
+    # r_j : [num_edges, 3]
+    def message(self, s_j, v_j, r_i, r_j):
+        phi = self.linear_phi1(s_j) # [num_edges, num_embeddings]
+        phi = F.silu(phi)
+        phi = self.linear_phi2(phi) # [num_edges, 3*num_embeddings]
+
+        rel_pos = r_i - r_j         # [num_edges, 3]
+        distance = torch.norm(rel_pos, dim=1) # [num_edges]
+        cutoff = distance.detach().clone()
+        cutoff[distance <= self.cutoff_dist] = 0.5*(torch.cos(torch.pi * distance[distance <= self.cutoff_dist] / self.cutoff_dist) + 1)
+        cutoff[distance > 0] = 0
+        RBF = cutoff[:, None] * torch.sin(torch.arange(1, 21, device=self.device)[None, :] * torch.pi * distance[:, None] / self.cutoff_dist) / distance[:, None] # [num_edges, 20]
+        W = self.linear_W(RBF)      # [num_edges, 3*num_embeddings]
+
+        split = torch.mul(phi, W)   # [num_edges, 3*num_embeddings]
+        split1 = split[:, :self.num_embeddings]
+        split2 = split[:, self.num_embeddings:2*self.num_embeddings]
+        split3 = split[:, 2*self.num_embeddings:]
+
+        delta_s_ij = split1 # [num_edges, num_embeddings]
+
+        v_j = v_j.permute([1,2,0]) # [num_edges, num_embeddings, 3]
+        delta_v_ij = torch.mul(v_j, split2[:, :, None]) \
+                            + torch.mul(torch.mul(split3[:, :, None], rel_pos[:, None, :]), distance[:, None, None]) # [num_edges, num_embeddings, 3]
+        delta_v_ij = delta_v_ij.permute([2, 0, 1]) # [3, natoms, num_embeddings]
+
+        return delta_s_ij, delta_v_ij
+
+    # _j is a neighbour, _i is the atom
+    # s_i : [num_edges, num_embeddings]
+    # v_i : [3, num_edges, num_embeddings]
+    def aggregate(self, message, neighbours):
+        delta_s_ij, delta_v_ij = message
+
+        delta_s = scatter(delta_s_ij, neighbours[1], dim=0, reduce="sum") # [natoms, num_embeddings]
+        delta_v = scatter(delta_v_ij, neighbours[1], dim=1, reduce="sum") # [3, natoms, num_embeddings]
+
+        return delta_s, delta_v
+
+    def update(self, agg_message, s, v):
+        delta_s, delta_v = agg_message
+        U = self.linear_U(delta_v) # [3, natoms, num_embeddings]
+        V = self.linear_V(delta_v) # [3, natoms, num_embeddings]
+
+        stack = torch.cat([delta_s, torch.norm(V, dim=0)], dim=1) # [natoms, 2*num_embeddings]
+
+        stack = self.linear_update1(stack) # [natoms, num_embeddings]
+        stack = F.silu(stack)
+        split = self.linear_update2(stack) # [natoms, 3*num_embeddings]
+
+        split1 = split[:, :self.num_embeddings]  # First part, contains the first 128 elements in the second dimension
+        split2 = split[:, self.num_embeddings: 2*self.num_embeddings]  # Second part, contains the next 128 elements
+        split3 = split[:, 2*self.num_embeddings:]  # Third part, contains the last 128 elements
+
+        delta_s = split2 + torch.sum(U * V, dim=0) * split3 # [natoms, num_embeddings]
+
+        delta_v = torch.mul(U, split1[None, :, :]) # [3, natoms, num_embeddings]
+
+        s += delta_s
+        v += delta_v
+
+        return s, v
+
+    def get_neighbours_as_edge_index(self, pos, batch, cutoff_dist):
+        # count atoms in each molecule
+        unique = torch.unique(batch, return_counts=True)
+        # i-th row is for i-th atom in data.z, ij element is distance between i-th and j-th atom
+        distances = torch.cdist(pos, pos, p=2)
+        # select atoms
+        neighbours = torch.where(distances <= cutoff_dist, 1, 0)
+        # mask other atoms
+        neighbours = neighbours * torch.block_diag(*[torch.ones((u,u)) for u in unique[1]]).to(self.device)
+        # exclude itself
+        neighbours = neighbours - torch.eye(neighbours.shape[0], device=self.device)
+        # get neighbours in the form of [2, num_edges]
+        neighbours = neighbours.nonzero(as_tuple=False).t()
+        neighbours = torch.index_select(neighbours, dim=0, index=torch.tensor([1,0], device=self.device)).type(torch.LongTensor).to(self.device)
+
+        return neighbours
+
 class PaiNN(nn.Module):
     def __init__(self, num_atoms, num_embeddings, cutoff_dist, hidden_out_dim, device, message_layers=3):
         super().__init__()
@@ -140,9 +252,11 @@ class PaiNN(nn.Module):
         self.device = device
         self.message_layers = message_layers
 
-        self.embeddings = nn.Embedding(num_atoms, num_embeddings)
+        self.embeddings = nn.Embedding(num_atoms, num_embeddings, padding_idx=0)
         self.message = Message(num_embeddings, cutoff_dist, device)
         self.update = Update(num_embeddings, cutoff_dist, device)
+
+        self.messagelayer = MessageLayer(num_embeddings, cutoff_dist, device)
 
         self.linear_out1 = nn.Linear(num_embeddings, hidden_out_dim)
         self.linear_out2 = nn.Linear(hidden_out_dim, 1)
@@ -154,8 +268,9 @@ class PaiNN(nn.Module):
 
         # 2. Send messages and make updates
         for _ in range(self.message_layers):
-            embeddings, equivariant_repr = self.message(embeddings, equivariant_repr, data.pos, data.batch)
-            embeddings, equivariant_repr = self.update(embeddings, equivariant_repr, data.pos, data.batch)
+            embeddings, equivariant_repr = self.messagelayer(embeddings, equivariant_repr, data.pos, data.batch)
+            # embeddings, equivariant_repr = self.message(embeddings, equivariant_repr, data.pos, data.batch)
+            # embeddings, equivariant_repr = self.update(embeddings, equivariant_repr, data.pos, data.batch)
 
         # 3. Final linear layer
         out = self.linear_out1(embeddings) # [batch_size, num_embeddings] -> [batch_size, hidden_out_dim]
@@ -171,7 +286,7 @@ if __name__ == "__main__":
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
     ### load data
-    dataset = QM9(root=f"./data")
+    dataset = QM9(root=f"/home/mikk/Deep_learning_2023/data")
 
     # Calculate split lengths
     total_length = len(dataset)
@@ -192,12 +307,27 @@ if __name__ == "__main__":
 
     ### Testing
     # Instantiate the PaiNN model
-    model = PaiNN(num_atoms=9, num_embeddings=128, cutoff_dist=5, hidden_out_dim=128, device=device) # Adjust the parameters as needed
+    model = PaiNN(num_atoms=10, num_embeddings=128, cutoff_dist=5, hidden_out_dim=128, device=device) # Adjust the parameters as needed
     model.to(device)
-    batch = next(iter(train_loader))
-    batch.to(device)
-    print(device)
-    print(batch)
 
-    out = model(batch)
-    print(out.shape)
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    torch.autograd.set_detect_anomaly(True)
+    for epoch in range(2):
+        model.train()
+        total_train_loss = 0.0
+        for batch in train_loader:
+            batch.to(device)
+            print(batch)
+            optimizer.zero_grad()
+            output = model(batch)
+            print(output.shape)
+            print(output.squeeze())
+            loss = criterion(output, batch.y[:, 1, None])
+            print(loss)
+            print(batch.y[:, 1, None])
+            loss.backward()
+            optimizer.step()
+            # total_train_loss += loss.item()
+        # avg_train_loss = total_train_loss / len(train_loader)
+        # print(f'Epoch [{epoch+1}/{2}], Train Loss: {avg_train_loss:.4f}')
--- cafa-cloud/main.py
+++ cafa-cloud/main.py
@@ -1,21 +1,13 @@
 import sys
 import os
-
-# getting the name of the directory
-# where the this file is present.
-current = os.path.dirname(os.path.realpath(__file__))
-
-# Getting the parent directory name
-# where the current directory is present.
-parent = os.path.dirname(current)
-
-# adding the parent directory to
-# the sys.path.
-sys.path.append(parent)
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
 import torch
+import torch.nn as nn
+import torch.optim as optim
 from torch_geometric.datasets import QM9
 from torch_geometric.loader import DataLoader
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 import wandb
 
 from PaiNN import PaiNN
@@ -24,7 +16,9 @@ from PaiNN import PaiNN
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 ### load data
-dataset = QM9(root=f"./data")
+dataset = QM9(root=f"/home/mikk/Deep_learning_2023/data")
+save_path = "/home/mikk/Deep_learning_2023/models/cafa01"
+name = "cafa01"
 
 # Calculate split lengths
 total_length = len(dataset)
@@ -41,16 +35,102 @@ train_set, val_set, test_set = torch.utils.data.random_split(dataset,
 train_loader = DataLoader(train_set, batch_size=32)
 val_loader = DataLoader(val_set, batch_size=32)
 test_loader = DataLoader(test_set, batch_size=32)
+print("Data loaded and split")
 
+### Hyperparameters
+param          = 1
+epochs         = 1
+batch_size     = 32
+num_atoms      = 10
+num_embeddings = 128
+cutoff_dist    = 5
+hidden_out_dim = 128
 
-### Testing
+### wandb setup
+wandb.login()
+wandb.init(project="PaiNN", name="cafa", config={
+    "param": param,
+    "epochs": epochs,
+    "batch_size": batch_size,
+    "num_atoms": num_atoms,
+    "num_embeddings": num_embeddings,
+    "cutoff_disc": cutoff_dist,
+    "criterion": "MSELoss",
+    "Optimizer": "Adam0001",
+    "device": str(device)
+})
+
+def root_mean_squared_error(preds, targets):
+    return torch.sqrt(torch.mean((preds - targets) ** 2))
+
+### Training
 # Instantiate the PaiNN model
-model = PaiNN(num_atoms=9, num_embeddings=128, cutoff_dist=5, hidden_out_dim=128, device=device) # Adjust the parameters as needed
+model = PaiNN(num_atoms=10, num_embeddings=128, cutoff_dist=5, hidden_out_dim=128, device=device) # Adjust the parameters as needed
 model.to(device)
-batch = next(iter(train_loader))
-batch.to(device)
-print(device)
-print(batch)
 
-out = model(batch)
-print(out.shape)
+criterion = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+
+print("Start training")
+
+scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=5, verbose=True)
+# Variable to keep track of smoothed validation loss
+smoothed_val_loss = None
+smoothing_factor = 0.9
+
+for epoch in range(epochs):
+    model.train()
+    total_train_loss = 0.0
+    total_train_rmse = 0.0
+
+    for batch in train_loader:
+        batch.to(device)
+        optimizer.zero_grad()
+        #Forward pass
+        output = model(batch)
+        # Assuming 'output' and 'batch.y' are aligned for loss calculation
+        loss = criterion(output.squeeze(), batch.y[:, param])
+        loss.backward()
+        optimizer.step()
+        print(batch)
+        print(output.squeeze())
+        total_train_loss += loss.item()
+        total_train_rmse += root_mean_squared_error(output.squeeze(), batch.y[:, param])
+
+    avg_train_loss = total_train_loss / len(train_loader)
+    print(f'Epoch [{epoch+1}/{epochs}], Train Loss: {avg_train_loss:.4f}')
+
+    # Validation phase
+    model.eval()
+    total_val_loss = 0.0
+    total_val_rmse = 0.0
+
+    with torch.no_grad():
+        for batch in val_loader:
+            batch.to(device)
+            output = model(batch)
+            loss = criterion(output.squeeze(), batch.y[:, param])
+            total_val_loss += loss.item()
+            total_val_rmse += root_mean_squared_error(output.squeeze(), batch.y[:, param])
+
+
+    avg_val_loss = total_val_loss / len(val_loader)
+    # Apply exponential smoothing to validation loss
+    if smoothed_val_loss is None:
+        smoothed_val_loss = avg_val_loss
+    else:
+        smoothed_val_loss = (smoothing_factor * smoothed_val_loss) + ((1 - smoothing_factor) * avg_val_loss)
+
+    print(f'Epoch [{epoch+1}/{epochs}], Train Loss: {avg_train_loss:.4f}, Smoothed Validation Loss: {smoothed_val_loss:.4f}')
+
+    # Adjust learning rate based on smoothed validation loss
+    scheduler.step(smoothed_val_loss)
+
+    wandb.log({"train_loss": avg_train_loss, "val loss": avg_train_loss, "smoothed val loss":smoothed_val_loss })
+    if (epoch + 1) % 10 == 0:
+        # Save the model
+        torch.save(model.state_dict(), f"{save_path}/{name}_epoch_{epoch+1}.pth")
+
+torch.save(model.state_dict(), f"{save_path}/{name}_final.pth")
+wandb.finish()
